1a4e3268fd1e5e93bbb26b16d90a90f73bda0873
Paulo Marcelo <marcelomp97447@gmail.com>
2026-01-17 20:30:14 -0300

    adicionando modelo de deck

---
+++ src/models/deck.py
@@ -0,0 +1,15 @@
+from datetime import datetime
+from beanie import Document, Link
+from typing import List
+from card import Card
+from enums.enums import DeckFormat
+
+class Deck(Document):
+    name: str
+    format: DeckFormat
+    created_at: datetime = datetime.now()
+    owner: Link["User"] 
+    cards: List[Link[Card]] = []
+
+    class Settings:
+        name = "decks"
