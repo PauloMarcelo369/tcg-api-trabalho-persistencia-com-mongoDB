ac751f3a4108679bd35538e9019e5458b8d0c144
Paulo Marcelo <marcelomp97447@gmail.com>
2026-01-18 03:30:58 -0300

    adicionando script de inicialização

--- src/models/user.py
+++ src/models/user.py
@@ -1,12 +1,13 @@
 from datetime import datetime
 from beanie import Document
 from typing import List
+from pydantic import Field
 
 class User(Document):
     name: str
     email: str
     password: str
-    created_at: datetime = datetime.now()
+    created_at: datetime = Field(default_factory=datetime.utcnow)
 
     class Settings:
         name = "users"
